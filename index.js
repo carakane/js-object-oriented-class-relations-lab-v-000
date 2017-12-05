ccfc419f07d8ad0b4de08a9b3c100e2f439f0d93
Cara Kane <cara.kane@gmail.com>
2017-12-05 19:36:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -41,8 +41,8 @@ let tripId = 0
 
 class Trip {
   constructor(driver, passenger) {
-    this.driver = driver
-    this.passenger = passenger
+    this.driverId = driver
+    this.passengerId = passenger
     this.id = ++tripId
     store.trips.push(this)
   }
