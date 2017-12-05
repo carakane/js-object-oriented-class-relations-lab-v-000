f152aa76c8e64111e62c777a277bc450ef45b361
Cara Kane <cara.kane@gmail.com>
2017-12-05 20:08:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ class Driver {
   }
 
   passengers() {
-    return(this.trips().forEach(trip => store.passengers.find(passenger => passenger.id === trip.passengerId)))
+    console.log(this.trips().forEach(trip => store.passengers.find(passenger => passenger.id === trip.passengerId)))
     // return(store.passengers.find(passenger => passenger.id === this.trips()[0].passengerId))
   }
 }
