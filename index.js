498c312206f91f8f702428e23338fdf0551706ff
Cara Kane <cara.kane@gmail.com>
2017-12-05 19:27:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,10 +7,46 @@ class Driver {
   }
 
   trips() {
-    
+
   }
 
   passengers() {
-    
+
+  }
+}
+
+let passengerId = 0
+
+class Passenger {
+    constructor(name) {
+      this.name = name
+      this.id = ++passengerId
+    }
+
+    trips() {
+
+    }
+
+    drivers() {
+
+    }
+
+}
+
+let tripId = 0
+
+class Trip {
+  constructor(driver, passenger) {
+    this.driver = driver
+    this.passenger = passenger
+    this.id = ++tripId
+  }
+
+  driver() {
+
+  }
+
+  passenger() {
+
   }
 }
